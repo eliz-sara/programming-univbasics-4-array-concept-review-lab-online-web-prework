3aa8408240ed33c736e861314a8f30e4ef8a1ee8
Sara Antoine <blythejolei@gmail.com>
2019-10-26 07:09:51 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,23 +4,12 @@ def find_element_index(array, value_to_find)
     return count
 end
 
-end
-
-nil
-
-end
 
 def find_max_value(array)
   max = 0
   array.length.times do |count|
     if array[count] > max
       max = array[count]
-end
-
-end
-
-max
-
 end
 
 def find_min_value(array)
